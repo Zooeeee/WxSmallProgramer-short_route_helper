412f6210f5f2d63ac4c3f31f1b5cef7bf451c72b
[Zooeeee] <594498431@qq.com>
2018-12-26 13:35:10 +0800

    搜索页面css初步完成

--- pages/search/search.js
+++ pages/search/search.js
@@ -6,22 +6,24 @@ Page({
      */
     data: {
         pickArrayIndex: 0,
-        pickArray: ["生活服务", "休闲娱乐",  " 健身", "旅游景点", "自然地物 ", "美食",
+        pickArray: ["生活服务", "休闲娱乐", " 健身", "旅游景点", "自然地物 ", "美食",
             " 宾馆 ", "购物 ", "汽车服务", "运动", "医疗", "交通设施"],
-        imgArray:[
-            {src:"./img/shenghuofuwu.png",value:"生活服务"},
-            {src:"./img/yule.png",value:"休闲娱乐"},
-            {src:"./img/jianshen.png",value:"健身"},
-            {src:"./img/jingdian.png",value:"旅游景点"},
-            {src:"./img/ziran.png",value:"自然地物"},
-            {src:"./img/meishi.png",value:"美食"},
-            {src:"./img/binguan.png",value:"宾馆"},
-            {src:"./img/gouwu.png",value:"购物"},
-            {src:"./img/qichefuwu.png",value:"汽车服务"},
-            {src:"./img/yundong.png",value:"运动"},
-            {src:"./img/yiliao.png",value:"医疗"},
-            {src:"./img/jiaotong.png",value:"交通设施"}
-        ]
+        imgArray: [
+            { src: "./img/shenghuofuwu.png", value: "生活服务" },
+            { src: "./img/yule.png", value: "休闲娱乐" },
+            { src: "./img/jianshen.png", value: "健身" },
+            { src: "./img/jingdian.png", value: "旅游景点" },
+            { src: "./img/ziran.png", value: "自然地物" },
+            { src: "./img/meishi.png", value: "美食" },
+            { src: "./img/binguan.png", value: "宾馆" },
+            { src: "./img/gouwu.png", value: "购物" },
+            { src: "./img/qichefuwu.png", value: "汽车服务" },
+            { src: "./img/yundong.png", value: "运动" },
+            { src: "./img/yiliao.png", value: "医疗" },
+            { src: "./img/jiaotong.png", value: "交通设施" },
+        ],
+        click: 0,
+        tipsIsShow:false
     },
 
     /**
@@ -44,8 +46,8 @@ Page({
     onShow: function () {
         let that = this;
         this.setData({
-            list:[],
-            input:"",
+            list: [],
+            input: "",
             pickArrayIndex: 0
         })
         wx.getStorage({//获取本地缓存
@@ -53,7 +55,7 @@ Page({
             success: function (res) {
                 that.setData({
                     defaultCity: res.data,
-                    class : that.data.pickArray[0]
+                    class: that.data.pickArray[0]
                 })
             }
         });
@@ -96,14 +98,6 @@ Page({
 
     },
 
-    pickChange: function (e) {
-        //console.log(e.detail.value)
-        this.setData({
-            pickArrayIndex: e.detail.value,
-            class: this.data.pickArray[e.detail.value]
-        });
-    },
-
     inputInput: function (e) {
         this.setData({
             input: e.detail.value
@@ -118,29 +112,53 @@ Page({
                 "&tag=" + that.data.class +
                 "&keyWord=" + that.data.input,
             success: function (res) {
-                console.log("picker:" + that.data.class);
+                /* console.log("picker:" + that.data.class);
                 console.log("input内容:" + that.data.input);
                 console.log("请求成功");
-                console.log(res.data.pointList);
+                console.log(res.data.pointList); */
+                if (res.data.pointList.length == 0) {
+                    console.log("警报");
+                    wx.showToast({
+                        title: '没有找到该地点',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
+                else
                 that.setData({
-                    list:res.data.pointList
+                    list: res.data.pointList,
+                    tipsIsShow:true
                 })
             }
         })
 
     },//搜索结束
-    routeHelp:function(e){
+    //路径帮助
+    routeHelp: function (e) {
         let address = this.data.list[e.target.id].address;
         wx.showModal({
             title: '地址',
-            content:address,
+            content: address,
             success: function (res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
-              } else {
-                console.log('用户点击取消')
-              }
+                if (res.confirm) {
+                    console.log('用户点击确定')
+                } else {
+                    console.log('用户点击取消')
+                }
             }
-          });
+        });
+    },
+    //切换选项 并执行搜索
+    optionClick: function (e) {
+        console.log(e.currentTarget.dataset.text);
+        let a = e.currentTarget.dataset.text;
+        this.setData({
+            class: this.data.imgArray[a].value,
+            click: e.currentTarget.dataset.text
+        });
+        this.search();
     }
+
+
+
 })
