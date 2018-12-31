148181c2e1725d89b0d4c230171878fc687aa3ec
[Zooeeee] <594498431@qq.com>
2018-12-31 10:38:12 +0800

    完成折线图的实时刷新

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ App({
     //系统初始化时 获取gps生成一个城市（市级单位）
     onLaunch: function (e) {
         console.log("系统第一次初始化");
-    }
+    },
+
 })
--- pages/prediction/prediction.js
+++ pages/prediction/prediction.js
@@ -1,84 +1,25 @@
 import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
-
-function initChart(canvas, width, height) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    title: {
-      text: '',
-      left: 'center'
-    },
-    color: ["red", "blue"],
-    legend: {
-      data: ['最高温', '最低温'],
-      top: 50,
-      left: 'center',
-      backgroundColor: 'white',
-      z: 100
-    },
-    grid: {
-      containLabel: true
-    },
-    tooltip: {
-      show: true,
-      trigger: 'axis'
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['一', '二', '三', '四'],
-      // show: false
-    },
-    yAxis: {
-      x: 'center',
-      type: 'value',
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      }
-      // show: false
-    },
-    series: [{
-      name: '最高温',
-      type: 'line',
-      smooth: true,
-      data: [-5, 22, 30, 30]
-    }, {
-      name: '最低温',
-      type: 'line',
-      smooth: true,
-      data: [12, -20, 41, 35]
-    }]
-  };
-
-  chart.setOption(option);
-  return chart;
-}
+var dataList = {};
+var k = 0;
+var Chart = null;
 // pages/prediction/prediction.js
 Page({
     data: {
         defaultCity: '武汉',
         totalHelpData: [],
         totalWeatherData: [],
-        dates : [],
+        dates: [],
+        tempsMax: [],
+        tempsMin: [],
         ec: {
-            onInit: initChart
-          }
+            lazyLoad: true // 延迟加载
+        }
     },
     //onload函数处理wxml中关于时间的部分 不需要刷新所以放在onload中
     onLoad: function (e) {
-        let that = this;
-        let dates = require('../../utils/getNearDate');
-        this.setData({
-            dates: dates.getNearDate()
-        })
+
     },
     //页面初次渲染完成
     onReady: function () {
@@ -87,17 +28,22 @@ Page({
     //切换页面时触发该函数
     onShow: function () {
         let that = this;
+        let dates = require('../../utils/getNearDate');
+        this.setData({
+            dates: dates.getNearDate()
+        })
         //请求缓存并将数据通过处理后放到 prediction.wxml上
-        wx.getStorage({
-            key: 'getData',
-            success: function (res) {
+        //同步加载数据   顺序才对
+        try {
+            let res = wx.getStorageSync('getData')
+            if (res) {
                 console.log("onShow中的请求缓存")
+                console.log(res);
                 //获取temps winds weathers开始
-                console.log(res.data)
                 let temps = []; let weathers = []; let winds = [];
-                for (let i in res.data.results[0].weather_data) {
-                    let tp = res.data.results[0].weather_data
-                    let temp = res.data.results[0].weather_data[i].temperature.split(" ");
+                for (let i in res.results[0].weather_data) {
+                    let tp = res.results[0].weather_data
+                    let temp = res.results[0].weather_data[i].temperature.split(" ");
                     //console.log(temp);
                     let max = temp[0] + "°C";
                     let min = temp[temp.length - 1];
@@ -108,8 +54,8 @@ Page({
 
                 //获取 title zs des 的开始
                 let titles = []; let zss = []; let dess = [];
-                for (let i in res.data.results[0].index) {
-                    let temp = res.data.results[0].index;
+                for (let i in res.results[0].index) {
+                    let temp = res.results[0].index;
                     titles.push(temp[i].title);
                     zss.push(temp[i].zs);
                     dess.push(temp[i].des);
@@ -117,7 +63,11 @@ Page({
                 // //获取 title zs des 的结束
                 //设置结合到一起数据 方便循环生成
                 let totalWeatherData = [];
+                let tempsMax = [];
+                let tempsMin = [];
                 for (let i = 0; i < temps.length; i++) {
+                    tempsMax.push(temps[i].max.replace("°C", ""));
+                    tempsMin.push(temps[i].min.replace("℃", ""));
                     totalWeatherData.push({
                         max: temps[i].max,
                         min: temps[i].min,
@@ -133,16 +83,94 @@ Page({
                         des: dess[i],
                     })
                 };
-                console.log(totalHelpData);
-                totalHelpData[4].title='紫外线';
+                totalHelpData[4].title = '紫外线';
                 that.setData({
                     totalWeatherData: totalWeatherData,
-                    totalHelpData: totalHelpData
+                    totalHelpData: totalHelpData,
+                    tempsMax: tempsMax,
+                    tempsMin: tempsMin
                 });
-
-            },
-        })
+            }
+          } catch (e) {
+            console.log(e)
+          }
+        this.echartsComponnet = this.selectComponent('#mychart');
+        this.getData(); //获取数据
     },
+    //图表绘制部分
+    getData: function () {
+        //如果是第一次绘制
+        let datas = this.data;
+        dataList = {
+            dates: this.data.dates,
+            tempsMax: this.data.tempsMax,
+            tempsMin: this.data.tempsMin
+        };
+        if (!Chart) {
+            console.log("初始化图表")
+            this.init_echarts(); //初始化图表
+        } else {
+            console.log("更新数据")
+            this.setOption(Chart); //更新数据
+        }
+    },
+    //初始化图表
+    init_echarts: function () {
+        this.echartsComponnet.init((canvas, width, height) => {
+            // 初始化图表
+            Chart = echarts.init(canvas, null, {
+                width: width,
+                height: height
+            });
+            // Chart.setOption(this.getOption());
+            this.setOption(Chart);
+            // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+            return Chart;
+        });
+    }, 
+    setOption: function (Chart) {
+        Chart.clear();  // 清除
+        console.log("清除旧图");
+        Chart.setOption(this.getOption());  //获取新数据
+        console.log("获取新数据");
+    },
+    getOption: function () {
+        // 指定图表的配置项和数据
+        var option = {
+            xAxis: {
+                type: 'category',
+                data: dataList.dates
+            },
+            tooltip: {
+                show: true,
+                trigger: 'axis'
+            },
+            yAxis: {
+                x: 'center',
+                type: 'value',
+                splitLine: {
+                    lineStyle: {
+                        type: 'dashed'
+                    }
+                }
+                // show: false
+            },
+            series: [{
+                name: '最高温',
+                type: 'line',
+                smooth: true,
+                data: dataList.tempsMax
+              }, {
+                name: '最低温',
+                type: 'line',
+                smooth: true,
+                data: dataList.tempsMin
+              }]
+        }
+        return option;
+    },
+
+
     //帮助消息绑定事件
     clickHelp: function (e) {
         let index = e.target.id;
@@ -159,7 +187,8 @@ Page({
                 }
             }
         });
-    }
+    },
+    
 })
 
 
